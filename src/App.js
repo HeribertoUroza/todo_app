50c9123d432bc059df14ae93f63ca6eaf55da925
Heriberto Uroza <heribertouroza@pursuit.org>
2019-02-03 14:53:30 -0500

    Added Components

--- src/App.js
+++ src/App.js
@@ -1,6 +1,9 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component, Fragment } from 'react';
+import { Col, Container, Row } from 'reactstrap';
+
+import { addTodo, removeTodo, updateTodo } from './todolist';
+import { TodoInput } from './Components/TodoInput';
+import { TodoList } from './Components/TodoList';
 
 class App extends Component {
   constructor(props){
@@ -9,9 +12,14 @@ class App extends Component {
     this.state = {
       todos: [],
     }
-
-    this.addToList('testing', true, Date.now() + 1000*60*60*24)
+    
+    
   }
+
+  // componentDidMount(){
+  //   this.addToList('testing', true, Date.now() + 1000 * 60 * 60 * 24)
+
+  // }
 
   addToList(name, isCompleted, dueDate){
     const newTodos = addToDo(this.state.todos, name, isCompleted, dueDate)
@@ -20,14 +28,37 @@ class App extends Component {
     })
   }
 
+  removeFromList(index) {
+    const newTodos = removeTodo(this.state.todos, index);
+    this.setState({
+      todos: newTodos,
+    })
+  }
+
   render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          
-        </header>
-      </div>
-    );
+    console.log('todos', this.state.todos)
+    const Input = <Row>
+      <TodoInput onTodoEntered={(name, isCompleted, dueDate) => this.addToList(name, isCompleted, dueDate)} />
+    </Row>
+
+    let Todos = null;
+    if (this.state.todos.length > 0) {
+      Todos = <Fragment>
+        <Row>
+          <h1>Todos</h1>
+        </Row>
+        <Row>
+          <TodoList todos={this.state.todos} />
+        </Row>
+      </Fragment>
+    }
+
+    return <Container>
+      <br />
+      {Input}
+      <br />
+      {Todos}
+    </Container>;
   }
 }
 
